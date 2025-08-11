093b953fb64bcab0ae20664084ff315423ffd8de
Maurizio Kovacic <mauriziokovacic@users.noreply.github.com>
2025-08-11 11:52:53 +0200

    Update Code.cpp

--- Coding/FizzBuzz/Code.cpp
+++ Coding/FizzBuzz/Code.cpp
@@ -1,11 +1,3 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-
-
 void fizzBuzz(int n)
 {
     for (int i = 1; i <= n; ++i)
@@ -20,38 +12,4 @@ void fizzBuzz(int n)
             std::cout << i << std::endl;
         }
     }
-}
-
-int main()
-{
-    string n_temp;
-    getline(cin, n_temp);
-
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    fizzBuzz(n);
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
 }
